71f2d8ee4db45e68e9704ca2598011fbd9b72a04
pthao12 <22026519@vnu.edu.vn>
2023-03-05 00:40:02 +0700

    Hangman

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -17,7 +17,7 @@ using std::cin;
 int generateRandomNumber(const int min, const int max)
 {
     // TODO: Return a random integer number between min and max
-    int randomNumber = rand() % max + min;
+    int randomNumber = rand() % (max - min + 1) + min;
     return randomNumber;
 }
 
